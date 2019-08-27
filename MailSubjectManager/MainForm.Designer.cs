346f9252fad8b3a22c7c04b435c13894443b3df5
Amen Ayach <aayach@Netways.com>
2019-08-27 17:27:58 +0300

    Initial Commit

---
+++ MailSubjectManager/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MailSubjectManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.taskbarIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxNotify = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctxNotify.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // taskbarIcon
+            // 
+            this.taskbarIcon.ContextMenuStrip = this.ctxNotify;
+            this.taskbarIcon.Icon = global::MailSubjectManager.Properties.Resources.mailicon;
+            this.taskbarIcon.Text = "Mail Subject Manager";
+            this.taskbarIcon.Visible = true;
+            this.taskbarIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TaskbarIcon_MouseClick);
+            // 
+            // ctxNotify
+            // 
+            this.ctxNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.ctxNotify.Name = "ctxNotify";
+            this.ctxNotify.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = global::MailSubjectManager.Properties.Resources.mailicon;
+            this.Name = "MainForm";
+            this.Text = "Mail Subject Manager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ctxNotify.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon taskbarIcon;
+        private System.Windows.Forms.ContextMenuStrip ctxNotify;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ MailSubjectManager/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailSubjectManager
+{
+    public partial class MainForm : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        private bool allowClose = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyDown += M_GlobalHook_KeyDown;
+        }
+
+        private void M_GlobalHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            Text = $"Ctrl: {e.Control} Shift: {e.Shift} Alt: {e.Alt} Key: {e.KeyCode}";
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.KeyDown -= M_GlobalHook_KeyDown;
+
+            m_GlobalHook.Dispose();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Subscribe();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (allowClose)
+            {
+                Unsubscribe();
+            }
+            else
+            {
+                this.Hide();
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            allowClose = true;
+            this.Close();
+        }
+
+        private void TaskbarIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Visible = !this.Visible;
+            }
+        }
+    }
+}
